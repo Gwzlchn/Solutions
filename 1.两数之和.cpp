ad540a5d30b2c3810228bb321761e228a28f0a24
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-07-04 15:32:44 +0800

    first commit

---
+++ 1.两数之和.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j] == target){
+                    return vector<int>({i,j});
+                }
+            }
+        }
+        return {0,0};
+    }
+};
+
