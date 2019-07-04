5e52be869c71ccea4f48b80b337531baf61da13a
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-07-04 17:11:13 +0800

    leetcode 13

--- 13.罗马数字转整数.cpp
+++ 13.罗马数字转整数.cpp
@@ -6,7 +6,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        
+        int target[256];
+        target['I'] = 1;
+        target['V'] = 5;
+        target['X'] = 10;
+        target['L'] = 50;
+        target['C'] = 100;
+        target['D'] = 500;
+        target['M'] = 1000;
+        int res = 0;
+        for(int i=0;i<s.size();i++){
+
+              if(i < s.length()-1 && target[s[i+1]] > target[s[i]])
+                res -= target[s[i]];
+            else
+                res += target[s[i]]; 
+
+        }
+        return res;
     }
 };
 
