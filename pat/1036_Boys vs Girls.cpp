c712b5eb967954443f3385b421d70679a01d09cc
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-07-06 16:13:35 +0800

    1036

---
+++ pat/1036_Boys vs Girls.cpp
@@ -0,0 +1,31 @@
+// 题目描述
+// This time you are asked to tell the difference between the lowest grade of 
+// all the male students and the highest grade of all the female students.
+
+// 输入描述:
+// Each input file contains one test case.  Each case contains a positive integer N, 
+// followed by N lines of student information.  Each line contains a student's name, 
+// gender, ID and grade, separated by a space, where name and ID are strings of no more 
+// than 10 characters with no space, gender is either F (female) or M (male), and grade
+//  is an integer between 0 and 100.  It is guaranteed that all the grades are distinct.
+
+
+// 输出描述:
+// For each test case, output in 3 lines.  The first line gives the name and ID of 
+// the female student with the highest grade, and the second line gives that of the 
+// male student with the lowest grade.  The third line gives the difference gradeF-gradeM.  
+// If one such kind of student is missing, output "Absent" in the corresponding line, 
+// and output "NA" in the third line instead.
+
+// 输入例子:
+// 3
+// Joe M Math990112 89
+// Mike M CS991301 100
+// Mary F EE990830 95
+
+// 输出例子:
+// Mary EE990830
+// Joe Math990112
+// 6
+
+
