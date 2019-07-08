a346592d1acb14ac7ee372a71a9deaa7641573b8
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-07-08 11:15:15 +0800

    leetcode 38

---
+++ leetcode/38_checking.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string countAndSay(int n)
+{
+    switch (n)
+    {
+    case 0:return "";
+    case 1:return "1";
+    }
+    string temp = countAndSay(n - 1);
+    int size = temp.size();
+    string res = "";
+    int cur_cnt = 1;
+
+    for (int i = 1; i < size; i++)
+    {
+        if (temp[i] == temp[i - 1])
+        {
+            cur_cnt++;
+            continue;
+        }
+        else
+        {
+            string sub = to_string(cur_cnt) + temp[i - 1];
+            res += sub;
+            cur_cnt = 1;
+            //cout<<cur_cnt<<" "<<res<<endl;
+        }
+    }
+
+    res.append(to_string(cur_cnt));
+    res.append(1, temp[temp.length() - 1]);
+
+    return res;
+}
+
+int main()
+{
+    cout << countAndSay(6);
+}
