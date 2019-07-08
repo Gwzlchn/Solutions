a4bc8d1dfb81435ed87d0deaac9623e6f9bbf0f1
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-07-08 09:50:41 +0800

    leetcode 26

--- leetcode/26.删除排序数组中的重复项.cpp
+++ leetcode/26.删除排序数组中的重复项.cpp
@@ -6,7 +6,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        if(!nums.empty()){
+            int cnt = 0;
+            for(int i=0;i<nums.size();i++){
+                if(nums[cnt] != nums[i]){
+                    cnt++;
+                    nums[cnt] = nums[i];
+                    
+                }
+            }
+            return cnt +1;
+        }
+        return 0;
     }
 };
 
