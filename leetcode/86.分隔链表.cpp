a5dbce5d891c9bb2227fe909b9aa237f9a202f19
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-07-12 16:08:27 +0800

    leetcode 86

--- leetcode/86.分隔链表.cpp
+++ leetcode/86.分隔链表.cpp
@@ -11,10 +11,33 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//思路：两条链表，最后拼接
+
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
+        ListNode* below = new ListNode(-1);
+
+        ListNode* up = new ListNode(-1);
+
+        ListNode *below_head = below, *up_head = up;
+        while(head){
+            if(head->val >= x){
+                up->next = head;
+                up = up->next;
+            }else{
+                below->next = head;
+                below = below->next;
+            }
+            head = head->next;
+        }
         
+        up->next = NULL;
+        below->next = up_head->next;
+
+        return below_head->next;
+
     }
 };
 
