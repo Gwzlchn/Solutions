0482ee372f40cea926093e3603e3f8a9f7a8b7f4
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-07-13 21:09:20 +0800

    递归中序遍历

---
+++ leetcode/94.二叉树的中序遍历 递归.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (66.22%)
+ * Likes:    221
+ * Dislikes: 0
+ * Total Accepted:    40.2K
+ * Total Submissions: 60.6K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树，返回它的中序 遍历。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * 输出: [1,3,2]
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ret;
+        recu(root,ret);
+        return ret;
+    }
+
+    void recu(TreeNode* root,vector<int>& ret){
+        if(root==NULL) return;
+        
+        if(root->left) recu(root->left,ret);
+        ret.push_back(root->val);
+        if(root->right) recu(root->right,ret);
+    }
+};
+
