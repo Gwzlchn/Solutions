e5abc992383d4480c711bbea50fb1e6ec924c108
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2019-07-14 17:23:51 +0800

    leetcode 319

---
+++ leetcode/234.回文链表.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=cpp
+ *
+ * [234] 回文链表
+ *
+ * https://leetcode-cn.com/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (37.54%)
+ * Likes:    237
+ * Dislikes: 0
+ * Total Accepted:    29.2K
+ * Total Submissions: 77.5K
+ * Testcase Example:  '[1,2]'
+ *
+ * 请判断一个链表是否为回文链表。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->2
+ * 输出: false
+ * 
+ * 示例 2:
+ * 
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 
+ * 
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+    }
+};
+
