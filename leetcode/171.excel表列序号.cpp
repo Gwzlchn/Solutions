094d1ec865b6cd1a29c4b3301d7f7024730f9e0f
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2020-02-29 13:57:02 +0800

    leetcode 171

---
+++ leetcode/171.excel表列序号.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=171 lang=cpp
+ *
+ * [171] Excel表列序号
+ *
+ * https://leetcode-cn.com/problems/excel-sheet-column-number/description/
+ *
+ * algorithms
+ * Easy (64.56%)
+ * Likes:    73
+ * Dislikes: 0
+ * Total Accepted:    16.4K
+ * Total Submissions: 25.4K
+ * Testcase Example:  '"A"'
+ *
+ * 给定一个Excel表格中的列名称，返回其相应的列序号。
+ * 
+ * 例如，
+ * 
+ * ⁠   A -> 1
+ * ⁠   B -> 2
+ * ⁠   C -> 3
+ * ⁠   ...
+ * ⁠   Z -> 26
+ * ⁠   AA -> 27
+ * ⁠   AB -> 28 
+ * ⁠   ...
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: "A"
+ * 输出: 1
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "AB"
+ * 输出: 28
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "ZY"
+ * 输出: 701
+ * 
+ * 致谢：
+ * 特别感谢 @ts 添加此问题并创建所有测试用例。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int res = 0;
+        for(char ss:s){
+            int temp = ss - 'A' + 1;
+            res = res*26 + temp;
+        }
+        return res;
+    }
+};
+// @lc code=end
+
