c83363b17f1b53b258e781848a3f3a1b65aaa879
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2020-03-24 13:16:38 +0800

    leetcode 198

--- leetcode/198.打家劫舍.cpp
+++ leetcode/198.打家劫舍.cpp
@@ -39,7 +39,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        int size = nums.size();
+        if(size==0)return 0;
+        if(size==1)return nums[0];
 
+        int* dp = new int[size];
+        dp[0] = nums[0];dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<size;i++){
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[size-1];
     }
 };
 // @lc code=end
