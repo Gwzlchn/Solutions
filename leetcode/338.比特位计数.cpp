c9e7bd5be8637da73d57bfbec553e00942ad828d
Gwzlchn <wangzl1116@mails.jlu.edu.cn>
2020-03-24 15:22:28 +0800

    leetcode 338

--- leetcode/338.比特位计数.cpp
+++ leetcode/338.比特位计数.cpp
@@ -37,7 +37,12 @@
 class Solution {
 public:
     vector<int> countBits(int num) {
-        
+        vector<int> res(num+1,0);
+        res[0] = 0;
+        for(int i=1;i<=num;i++){
+            res[i] = res[i&(i-1)] + 1;
+        }
+        return res;
     }
 };
 
