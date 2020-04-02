b68b48fe22ecabe1ee349edabfb1b47474f3ac30
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-02 10:33:16 +0800

    leetcode 341 medium 广义表

---
+++ leetcode/341.扁平化嵌套列表迭代器.cpp
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode.cn id=341 lang=cpp
+ *
+ * [341] 扁平化嵌套列表迭代器
+ *
+ * https://leetcode-cn.com/problems/flatten-nested-list-iterator/description/
+ *
+ * algorithms
+ * Medium (62.28%)
+ * Likes:    76
+ * Dislikes: 0
+ * Total Accepted:    7K
+ * Total Submissions: 11.2K
+ * Testcase Example:  '[[1,1],2,[1,1]]'
+ *
+ * 给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+ * 
+ * 列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: [[1,1],2,[1,1]]
+ * 输出: [1,1,2,1,1]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,[4,[6]]]
+ * 输出: [1,4,6]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+// 思路类似于 广义表
+class NestedIterator {
+public:
+    vector<int> m_vector;
+    vector<int>::iterator m_iter;
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        for(auto& iter:nestedList){
+            parse_vector(iter);
+        }
+        m_iter = m_vector.begin();
+    }
+    void parse_vector(const NestedInteger& nest){
+        if(nest.isInteger()){
+            m_vector.emplace_back(nest.getInteger());
+        }else{
+            for(auto iter:nest.getList()){
+                parse_vector(iter);
+            }
+        }
+    }
+    int next() {
+        return *(m_iter++);
+    }
+    
+    bool hasNext() {
+        return m_iter != m_vector.end();
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
+// @lc code=end
+
