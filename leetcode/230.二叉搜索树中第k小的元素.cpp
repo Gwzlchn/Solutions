53c04bf4b895311df0fdcef4a62668faa1604876
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-03 11:08:12 +0800

    leetcode 230

---
+++ leetcode/230.二叉搜索树中第k小的元素.cpp
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=cpp
+ *
+ * [230] 二叉搜索树中第K小的元素
+ *
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (68.96%)
+ * Likes:    176
+ * Dislikes: 0
+ * Total Accepted:    44K
+ * Total Submissions: 63.3K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ * 
+ * 说明：
+ * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * 输出: 1
+ * 
+ * 示例 2:
+ * 
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * 输出: 3
+ * 
+ * 进阶：
+ * 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+stack<TreeNode*> m_st;
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        while(root){
+            m_st.push(root);
+            root = root->left;
+        }
+        
+        while(!m_st.empty()){
+            TreeNode* node = m_st.top();
+            m_st.pop();
+            if(--k == 0) return node->val;
+
+            node = node->right;
+            while(node){
+                m_st.push(node);
+                node = node->left;
+            }
+        }
+        return 0;
+    }
+};
+// @lc code=end
+
