3b7dd34f7e33999854a8f1ebcc2af9396c50053b
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-07 14:47:17 +0800

    leetcode 0107 other

--- leetcode/面试题 01.07. 旋转矩阵.cpp
+++ leetcode/面试题 01.07. 旋转矩阵.cpp
@@ -17,4 +17,21 @@ public:
     }
 };
 
-    
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int max_row = (n>>1)-1;
+        int max_col =(n-1)>>1;
+        for(int i=max_row;i>=0;i--){
+            for(int j=max_col;j>=0;j--){
+                tie(matrix[i][j],matrix[j][n-i-1],matrix[n-i-1][n-j-1],matrix[n-j-1][i]) = 
+                    make_tuple(matrix[n-j-1][i],matrix[i][j],matrix[j][n-i-1],matrix[n-i-1][n-j-1]);
+            }
+        }
+        return;
+        
+        
+    }
+};
