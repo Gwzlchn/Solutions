492868a0fa48ca8d2cc58e79c7c86deac5918220
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-15 15:42:11 +0800

    leetcode 542 medium

---
+++ leetcode/542.01-矩阵.cpp
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode.cn id=542 lang=cpp
+ *
+ * [542] 01 矩阵
+ *
+ * https://leetcode-cn.com/problems/01-matrix/description/
+ *
+ * algorithms
+ * Medium (37.93%)
+ * Likes:    213
+ * Dislikes: 0
+ * Total Accepted:    21.5K
+ * Total Submissions: 50.9K
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * 给定一个由 0 和 1 组成的矩阵，找出每个元素到最近的 0 的距离。
+ * 
+ * 两个相邻元素间的距离为 1 。
+ * 
+ * 示例 1: 
+ * 输入:
+ * 
+ * 
+ * 0 0 0
+ * 0 1 0
+ * 0 0 0
+ * 
+ * 
+ * 输出:
+ * 
+ * 
+ * 0 0 0
+ * 0 1 0
+ * 0 0 0
+ * 
+ * 
+ * 示例 2: 
+ * 输入:
+ * 
+ * 
+ * 0 0 0
+ * 0 1 0
+ * 1 1 1
+ * 
+ * 
+ * 输出:
+ * 
+ * 
+ * 0 0 0
+ * 0 1 0
+ * 1 2 1
+ * 
+ * 
+ * 注意:
+ * 
+ * 
+ * 给定矩阵的元素个数不超过 10000。
+ * 给定矩阵中至少有一个元素是 0。
+ * 矩阵中的元素只在四个方向上相邻: 上、下、左、右。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+private:
+    static constexpr int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> dist(m, vector<int>(n));
+        vector<vector<int>> seen(m, vector<int>(n));
+        queue<pair<int, int>> q;
+        // 将所有的 0 添加进初始队列中
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    q.emplace(i, j);
+                    seen[i][j] = 1;
+                }
+            }
+        }
+
+        // 广度优先搜索
+        while (!q.empty()) {
+            auto [i, j] = q.front();
+            q.pop();
+            for (int d = 0; d < 4; ++d) {
+                int ni = i + dirs[d][0];
+                int nj = j + dirs[d][1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && !seen[ni][nj]) {
+                    dist[ni][nj] = dist[i][j] + 1;
+                    q.emplace(ni, nj);
+                    seen[ni][nj] = 1;
+                }
+            }
+        }
+
+    return dist;
+    }
+};
+
+
+// @lc code=end
+
