0532c585408d8f75f472622bd6a7723859db6f46
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-21 11:07:23 +0800

    leetcode 560 medium

---
+++ leetcode/560.和为k的子数组.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=560 lang=cpp
+ *
+ * [560] 和为K的子数组
+ *
+ * https://leetcode-cn.com/problems/subarray-sum-equals-k/description/
+ *
+ * algorithms
+ * Medium (44.17%)
+ * Likes:    289
+ * Dislikes: 0
+ * Total Accepted:    23.9K
+ * Total Submissions: 53.7K
+ * Testcase Example:  '[1,1,1]\n2'
+ *
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+ * 
+ * 示例 1 :
+ * 
+ * 
+ * 输入:nums = [1,1,1], k = 2
+ * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+ * 
+ * 
+ * 说明 :
+ * 
+ * 
+ * 数组的长度为 [1, 20,000]。
+ * 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+
+    }
+};
+
+
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+        
+//         int len = nums.size();
+//         //if(!len) return 0;
+//         int cnt = 0;
+//         for(int i=0;i<len;i++){
+//             int sum = 0;
+//             for(int j=i;j<len;j++){
+//                 sum += nums[j];
+//                 if(sum == k){
+//                     cnt++;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+// @lc code=end
+
