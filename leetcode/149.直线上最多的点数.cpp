ee4be23ec244d5b058ba65a2b774becab3bf8eb9
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-21 16:32:23 +0800

    leetocde 149 medium

--- leetcode/149.直线上最多的点数.cpp
+++ leetcode/149.直线上最多的点数.cpp
@@ -45,8 +45,59 @@
  * 
  */
 
-class Solution {
-public:
+// function has to live in the std namespace 
+// so that it is picked up by argument-dependent name lookup (ADL).
+namespace std {
+	namespace
+	{
+
+		// Code from boost
+		// Reciprocal of the golden ratio helps spread entropy
+		//     and handles duplicates.
+		// See Mike Seymour in magic-numbers-in-boosthash-combine:
+		//     https://stackoverflow.com/questions/4948780
+
+		template <class T>
+		inline void hash_combine(std::size_t& seed, T const& v)
+		{
+			seed ^= hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+		}
+
+		// Recursive template code derived from Matthieu M.
+		template <class Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
+		struct HashValueImpl
+		{
+			static void apply(size_t& seed, Tuple const& tuple)
+			{
+				HashValueImpl<Tuple, Index - 1>::apply(seed, tuple);
+				hash_combine(seed, get<Index>(tuple));
+			}
+		};
+
+		template <class Tuple>
+		struct HashValueImpl<Tuple, 0>
+		{
+			static void apply(size_t& seed, Tuple const& tuple)
+			{
+				hash_combine(seed, get<0>(tuple));
+			}
+		};
+	}
+
+	template <typename ... TT>
+	struct hash<std::tuple<TT...>>
+	{
+		size_t
+			operator()(std::tuple<TT...> const& tt) const
+		{
+			size_t seed = 0;
+			HashValueImpl<std::tuple<TT...> >::apply(seed, tt);
+			return seed;
+		}
+
+	};
+}
+
 
 //求两个数最小公倍数
 int GCD(int a, int b)
@@ -73,7 +124,7 @@ int maxPoints(vector<vector<int>>& points) {
 
 	// key -> tuple<int,int,long long> => Ax + By + C = 0 记录的是 A B C
 	// value -> 坐标（i,j）即 出现在这条线上的点集
-	map<tuple<int, int, long long>, set<pair<int, int>>> point_in_line;
+	unordered_map<tuple<int, int, long long>, set<pair<int, int>>> point_in_line;
 
 	// 点集去重，并计数
 	map<pair<int, int>, int> unique_point;
@@ -137,6 +188,5 @@ int maxPoints(vector<vector<int>>& points) {
 
 
 }
-};
 // @lc code=end
 
