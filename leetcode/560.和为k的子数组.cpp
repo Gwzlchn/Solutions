3c94a9d5d0f6599ee1719c832db96516c130ae36
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-21 11:22:29 +0800

    leetcode 560 commet

--- leetcode/560.和为k的子数组.cpp
+++ leetcode/560.和为k的子数组.cpp
@@ -32,6 +32,12 @@
  */
 
 // @lc code=start
+
+
+这种方法背后的想法如下：如果累积总和（由 sum[i]sum[i] 表示加到 i^{th}ith的和）最多两个指数是相同的，那么这些元素之间的元素总和为零。进一步扩展相同的想法，如果累计总和，在索引 ii 和 jj 处相差 kk，即 sum[i] - sum[j] = ksum[i]−sum[j]=k，则位于索引 ii 和 jj 之间的元素之和是 kk。
+
+基于这些想法，可以使用了一个哈希表 mapmap，它用于存储所有可能的索引的累积总和以及相同累加和发生的次数。我们以以下形式存储数据：(sum_isum i​	 ，sum_isum i 的出现次数)。我们遍历数组numsnums并继续寻找累积总和。每当我们遇到一个新的和时，我们在hashmap中创建一个与该总和相对应的新条目。如果再次出现相同的和，我们增加与mapmap中的和相对应的计数。此外，对于遇到的每个总和，我们还确定已经发生 sum-ksum−k 总和的次数，因为它将确定具有总和 kk 的子阵列发生到当前索引的次数。我们将 countcount 增加相同的量。
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
