d3d0f660424d6a41c603ff007ba70648f2b373dc
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-27 21:36:02 +0800

    leetcode 69 easy

--- leetcode/69.x-的平方根.cpp
+++ leetcode/69.x-的平方根.cpp
@@ -37,7 +37,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
+        if (x < 2) return x;
+
+        double x0 = x;
+        double x1 = (x0 + x / x0) / 2.0;
+        while (abs(x0 - x1) >= 1) {
+            x0 = x1;
+            x1 = (x0 + x / x0) / 2.0;
+        }
+
+    return (int)x1;
+
     }
 };
-
